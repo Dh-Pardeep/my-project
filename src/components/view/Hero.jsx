527ddf54992ce367f0735879cddd95941ac482ab
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-01-09 11:12:08 +0530

    last commit

--- src/components/view/Hero.jsx
+++ src/components/view/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
           <div className='flex  flex-col sm:flex-row items-center justify-between py-2 pe-[9px] ps-[21px] max-w-[290px]  sm:max-w-[464px] mx-auto bg-white rounded-[100px]  '>
             <input type="text" placeholder='Enter email to join waitlist' className='w-10/12  text-center sm:text-start placeholder:font-Inter placeholder:sm:text-[14px] placeholder:text-base placeholder:font-normal placeholder:text-[#666666] mt-[10px] sm:mt-0 outline-none placeholder:leading-normal' />
             <div className='rounded-[35px] group  mt-[20px]  cursor-pointer sm:mt-0 bg-gradient-to-r from-cyan-500 to-blue-500 inline-block px-[27px] py-[16px] '>
-              <div className='group-hover:translate-x-[6px] transition-all ease-linear duration-200'>
+              <div className='group-hover:translate-x-[6px] transition-all ease-linear duration-300'>
                 <ArrowIcon />
               </div>
             </div>
